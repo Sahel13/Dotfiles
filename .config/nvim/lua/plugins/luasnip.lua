167db4cc27e5dfda90a454a4d14aa08e8d8d17ed
Sahel Iqbal <sahel13miqbal@proton.me>
2025-09-23 20:56:47 +0300

    Improve tab shortcut in luasnip

--- .config/nvim/lua/plugins/luasnip.lua
+++ .config/nvim/lua/plugins/luasnip.lua
@@ -17,6 +17,8 @@ return {
     vim.keymap.set({ "i", "s" }, "<tab>", function()
       if ls.expand_or_jumpable() then
         ls.expand_or_jump()
+      else
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", false)
       end
     end, { silent = true })
     vim.keymap.set({ "i", "s" }, "<s-tab>", function()
